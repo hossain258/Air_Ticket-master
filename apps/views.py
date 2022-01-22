e3e737d5ebc0af4b557a2870f58b4803e87aebde
hossain <hossain258@gmail.com>
2022-01-22 20:47:58 +0000

    1st commit

---
+++ apps/views.py
@@ -0,0 +1,364 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    diction = {}
+    return render(request, 'index.html', context=diction)
+
+def about(request):
+    diction = {}
+    return render(request, 'about.html', context=diction)
+
+def activity_search_result(request):
+    diction = {}
+    return render(request, 'activity-search-result.html', context=diction)
+
+def add_car(request):
+    diction = {}
+    return render(request, 'add-car.html', context=diction)
+
+def add_cruise(request):
+    diction = {}
+    return render(request, 'add-cruise.html', context=diction)
+
+def add_flight(request):
+    diction = {}
+    return render(request, 'add-flight.html', context=diction)
+
+def add_hotel(request):
+    diction = {}
+    return render(request, 'add-hotel.html', context=diction)
+
+def add_new_post(request):
+    diction = {}
+    return render(request, 'add-new-post.html', context=diction)
+
+def add_tour(request):
+    diction = {}
+    return render(request, 'add-tour.html', context=diction)
+
+def admin_airlines(request):
+    diction = {}
+    return render(request, 'admin-airlines.html', context=diction)
+
+def admin_countries(request):
+    diction = {}
+    return render(request, 'admin-countries.html', context=diction)
+
+def admin_currency_list(request):
+    diction = {}
+    return render(request, 'admin-currency-list.html', context=diction)
+
+def admin_dashboard_booking(request):
+    diction = {}
+    return render(request, 'admin-dashboard-booking.html', context=diction)
+
+def admin_dashboard_orders_details(request):
+    diction = {}
+    return render(request, 'admin-dashboard-orders-details.html', context=diction)
+
+def admin_dashboard_orders(request):
+    diction = {}
+    return render(request, 'admin-dashboard-orders.html', context=diction)
+
+def admin_dashboard_reviews(request):
+    diction = {}
+    return render(request, 'admin-dashboard-reviews.html', context=diction)
+
+def admin_dashboard_settings(request):
+    diction = {}
+    return render(request, 'admin-dashboard-settings.html', context=diction)
+
+def admin_dashboard_subscribers(request):
+    diction = {}
+    return render(request, 'admin-dashboard-subscribers.html', context=diction)
+
+def admin_dashboard_travel_agents(request):
+    diction = {}
+    return render(request, 'admin-dashboard-travel-agents.html', context=diction)
+
+def admin_dashboard_traveler_detail(request):
+    diction = {}
+    return render(request, 'admin-dashboard-traveler-detail.html', context=diction)
+
+def admin_dashboard_travellers(request):
+    diction = {}
+    return render(request, 'admin-dashboard-travellers.html', context=diction)
+
+def admin_dashboard_visa(request):
+    diction = {}
+    return render(request, 'admin-dashboard-visa.html', context=diction)
+
+def admin_dashboard_wishlist(request):
+    diction = {}
+    return render(request, 'admin-dashboard-wishlist.html', context=diction)
+
+def admin_dashboard(request):
+    diction = {}
+    return render(request, 'admin-dashboard.html', context=diction)
+
+def admin_invoice(request):
+    diction = {}
+    return render(request, 'admin-invoice.html', context=diction)
+
+def admin_payments(request):
+    diction = {}
+    return render(request, 'admin-payments.html', context=diction)
+
+def become_local_expert(request):
+    diction = {}
+    return render(request, 'become-local-expert.html', context=diction)
+
+def blog_full_width(request):
+    diction = {}
+    return render(request, 'blog-full-width.html', context=diction)
+
+def blog_grid(request):
+    diction = {}
+    return render(request, 'blog-grid.html', context=diction)
+
+def blog_sidebar(request):
+    diction = {}
+    return render(request, 'blog-sidebar.html', context=diction)
+
+def blog_single(request):
+    diction = {}
+    return render(request, 'blog-single.html', context=diction)
+
+def car_booking(request):
+    diction = {}
+    return render(request, 'car-booking.html', context=diction)
+
+def car_grid(request):
+    diction = {}
+    return render(request, 'car-grid.html', context=diction)
+
+def car_list(request):
+    diction = {}
+    return render(request, 'car-list.html', context=diction)
+
+def car_search_result(request):
+    diction = {}
+    return render(request, 'car-search-result.html', context=diction)
+
+def car_sidebar(request):
+    diction = {}
+    return render(request, 'car-sidebar.html', context=diction)
+
+def car_single(request):
+    diction = {}
+    return render(request, 'car-single.html', context=diction)
+
+def career_details(request):
+    diction = {}
+    return render(request, 'career-details.html', context=diction)
+
+def career(request):
+    diction = {}
+    return render(request, 'career.html', context=diction)
+
+def cart(request):
+    diction = {}
+    return render(request, 'cart.html', context=diction)
+
+def checkout(request):
+    diction = {}
+    return render(request, 'checkout.html', context=diction)
+
+def coming_soon(request):
+    diction = {}
+    return render(request, 'coming-soon.html', context=diction)
+
+def contact(request):
+    diction = {}
+    return render(request, 'contact.html', context=diction)
+
+def cruise_booking(request):
+    diction = {}
+    return render(request, 'cruise-booking.html', context=diction)
+
+def cruise_details(request):
+    diction = {}
+    return render(request, 'cruise-details.html', context=diction)
+
+def cruise_search_result(request):
+    diction = {}
+    return render(request, 'cruise-search-result.html', context=diction)
+
+def cruise_sidebar(request):
+    diction = {}
+    return render(request, 'cruise-sidebar.html', context=diction)
+
+def cruises_list(request):
+    diction = {}
+    return render(request, 'cruises-list.html', context=diction)
+
+def cruises(request):
+    diction = {}
+    return render(request, 'cruises.html', context=diction)
+
+def destinations(request):
+    diction = {}
+    return render(request, 'destinations.html', context=diction)
+
+def faq(request):
+    diction = {}
+    return render(request, 'faq.html', context=diction)
+
+def flight_booking(request):
+    diction = {}
+    return render(request, 'flight-booking.html', context=diction)
+
+def flight_grid(request):
+    diction = {}
+    return render(request, 'flight-grid.html', context=diction)
+
+def flight_list(request):
+    diction = {}
+    return render(request, 'flight-list.html', context=diction)
+
+def flight_search_result(request):
+    diction = {}
+    return render(request, 'flight-search-result.html', context=diction)
+
+def flight_sidebar(request):
+    diction = {}
+    return render(request, 'flight-sidebar.html', context=diction)
+
+def flight_single(request):
+    diction = {}
+    return render(request, 'flight-single.html', context=diction)
+
+def gallery(request):
+    diction = {}
+    return render(request, 'gallery.html', context=diction)
+
+def hotel_booking(request):
+    diction = {}
+    return render(request, 'hotel-booking.html', context=diction)
+
+def hotel_grid(request):
+    diction = {}
+    return render(request, 'hotel-grid.html', context=diction)
+
+def hotel_list(request):
+    diction = {}
+    return render(request, 'hotel-list.html', context=diction)
+
+def hotel_search_result(request):
+    diction = {}
+    return render(request, 'hotel-search-result.html', context=diction)
+
+def hotel_sidebar(request):
+    diction = {}
+    return render(request, 'hotel-sidebar.html', context=diction)
+
+def hotel_single(request):
+    diction = {}
+    return render(request, 'hotel-single.html', context=diction)
+
+def page_404(request):
+    diction = {}
+    return render(request, 'page-404.html', context=diction)
+
+def payment_complete(request):
+    diction = {}
+    return render(request, 'payment-complete.html', context=diction)
+
+def payment_received(request):
+    diction = {}
+    return render(request, 'payment-received.html', context=diction)
+
+def pricing(request):
+    diction = {}
+    return render(request, 'pricing.html', context=diction)
+
+def recover(request):
+    diction = {}
+    return render(request, 'recover.html', context=diction)
+
+def room_details(request):
+    diction = {}
+    return render(request, 'room-details.html', context=diction)
+
+def room_grid(request):
+    diction = {}
+    return render(request, 'room-grid.html', context=diction)
+
+def room_list(request):
+    diction = {}
+    return render(request, 'room-list.html', context=diction)
+
+def room_search_result_list(request):
+    diction = {}
+    return render(request, 'room-search-result-list.html', context=diction)
+
+def room_search_result(request):
+    diction = {}
+    return render(request, 'room-search-result.html', context=diction)
+
+def services(request):
+    diction = {}
+    return render(request, 'services.html', context=diction)
+
+def tour_booking(request):
+    diction = {}
+    return render(request, 'tour-booking.html', context=diction)
+
+def tour_details(request):
+    diction = {}
+    return render(request, 'tour-details.html', context=diction)
+
+def tour_fullwidth(request):
+    diction = {}
+    return render(request, 'tour-fullwidth.html', context=diction)
+
+def tour_grid(request):
+    diction = {}
+    return render(request, 'tour-grid.html', context=diction)
+
+def tour_left_sidebar(request):
+    diction = {}
+    return render(request, 'tour-left-sidebar.html', context=diction)
+
+def tour_list(request):
+    diction = {}
+    return render(request, 'tour-list.html', context=diction)
+
+def tour_right_sidebar(request):
+    diction = {}
+    return render(request, 'tour-right-sidebar.html', context=diction)
+
+def tour_search_result(request):
+    diction = {}
+    return render(request, 'tour-search-result.html', context=diction)
+
+def user_dashboard_booking(request):
+    diction = {}
+    return render(request, 'user-dashboard-booking.html', context=diction)
+
+def user_dashboard_profile(request):
+    diction = {}
+    return render(request, 'user-dashboard-profile.html', context=diction)
+
+def user_dashboard_reviews(request):
+    diction = {}
+    return render(request, 'user-dashboard-reviews.html', context=diction)
+
+def user_dashboard_settings(request):
+    diction = {}
+    return render(request, 'user-dashboard-settings.html', context=diction)
+
+def user_dashboard_wishlist(request):
+    diction = {}
+    return render(request, 'user-dashboard-wishlist.html', context=diction)
+
+def user_dashboard(request):
+    diction = {}
+    return render(request, 'user-dashboard.html', context=diction)
+
+def user_profile(request):
+    diction = {}
+    return render(request, 'user-profile.html', context=diction)
